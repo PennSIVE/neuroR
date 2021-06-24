de45fa071ea3c1985f21ee7deb97d103345616a9
neuroconductor-devel <tim.terf@gmail.com>
2021-06-24 10:20:41 -0400

    R 4.1 released

---
+++ 4.1-0.1.0/dockerfiles/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+docker pull repronim/neurodocker:master # get latest version
+docker run --rm repronim/neurodocker:master generate docker \
+    --pkg-manager apt \
+    --base debian:buster \
+    --run "apt-get update && apt-get install -y multiarch-support" \
+    --afni version=latest \
+    --ants version=2.3.4 \
+    --fsl version=6.0.4 \
+    --minc version=1.9.15 \
+    --freesurfer version=7.1.1 \
+    --convert3d version=1.0.0 \
+    --dcm2niix version=latest method=source \
+    | docker build -t pennsive/neurodocker:buster -
+# note: without multiarch-support dpkg will not be able to install the pre-dependancy libxp6
+
+docker build -t pennsive/neuror:4.1 .
+id=$(docker images --format="{{.Repository}}:{{.Tag}} {{.ID}}" | egrep "^pennsive/neuror:4.1 " | cut -d' ' -f2)
+docker tag $id pennsive/neuror
+docker push pennsive/neuror:4.1
+docker push pennsive/neuror
